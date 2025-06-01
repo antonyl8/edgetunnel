35d59bc800e43f1511e68bb83814401a4a84a039
antonyl8 <anthonylewis5920@gmail.com>
2025-06-01 16:02:37 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'a789cefa-e10f-41ee-a565-b65822f6a257';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
